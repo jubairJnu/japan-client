8d8c057186745a6dad42e43f9a17bac7031cdd7f
jubairJnu <jubair2810@gmail.com>
2024-12-12 23:26:47 +0600

    dashbord-home-page-added

--- src/app/(withDashbordLayout)/dashboard/page.tsx
+++ src/app/(withDashbordLayout)/dashboard/page.tsx
@@ -1,11 +1,11 @@
-
-
-const page = () => {
+const DashboardHomePage = () => {
   return (
-    <div>
-      
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-xl font-bold text-center text-violet-500">
+        Welcome to Admin Dashboard
+      </h1>
     </div>
   );
 };
 
-export default page;
+export default DashboardHomePage;
